496aba1e5b97ac11225a0cacfc360ec064a661e9
Tassawar-Abbas <tas80066@gmail.com>
2025-02-17 15:52:54 +0500

    header section for car rental

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+'use client'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { headerLinks } from '../constants/data'
+import { usePathname } from 'next/navigation'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    const pathname = usePathname();
+    const [showMenu, setShowMenu] = useState(false)
+    return (
+        <header className='bg-white shadow-lg h-16 flex justify-between items-center md:px-[5%]' >
+            <Link href='/' className='text-third-color font-bold text-2xl pl-3 md:pl-0'>Amahh Car Rental</Link>
+            <div className='md:hidden flex flex-col gap-1 pr-3 md:pr-0' onClick={() => setShowMenu(!showMenu)}>
+                <div className='w-5 h-0.5 bg-black'></div>
+                <div className='w-5 h-0.5 bg-black'></div>
+                <div className='w-5 h-0.5 bg-black'></div>
+            </div>
+            <div className={`flex ${showMenu?"flex-col absolute top-16 w-full":"hidden md:flex"} gap-5  md:flex-row md:static md:w-auto bg-white text-center`}>
+                {headerLinks.map((hLink) => (
+                    <div key={hLink.link}>
+                        <Link href={hLink.link} exact={hLink.exact}
+                            className={`${pathname == hLink.link ? 'text-third-color' : 'text-gray-500'}`}>
+                            <span>{hLink.text}</span>
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        </header>
+    )
 }
 
 export default Header
